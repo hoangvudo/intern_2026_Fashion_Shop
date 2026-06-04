f30edb3ee1b8c841fe7216bfdc8f8fc10c66a52b
quocdat1302 <letanquocdat132@gmail.com>
2026-06-04 14:56:06 +0700

    chore: xóa thư mục BE và dọn dẹp dự án

--- BE/src/main/java/com/fashion/backend/dto/CancelOrderRequest.java
+++
@@ -1,8 +0,0 @@
-package com.fashion.backend.dto;
-
-import lombok.Data;
-
-@Data
-public class CancelOrderRequest {
-    private String reason;
-}
--- BE/src/main/java/com/fashion/backend/entity/OrderStatus.java
+++
@@ -1,9 +0,0 @@
-package com.fashion.backend.entity;
-
-public enum OrderStatus {
-    PENDING,
-    CONFIRMED,
-    SHIPPING,
-    COMPLETED,
-    CANCELLED
-}
